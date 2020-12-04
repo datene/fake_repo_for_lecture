6d749efc4c13d2bbc8d93cabfb60a862f72c8bf8
Lars Bohm <larsbohm@gmail.com>
2020-12-04 10:02:03 +0100

    intiialize

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def full_name
+    "#{self.first_name.downcase.capitalize} #{self.last_name.downcase.capitalize}"
+  end
 end
